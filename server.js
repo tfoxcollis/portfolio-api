7651293ac09f2f006c6f7032f14f7af518609672
Trish Fox-Collis <tfoxcollis@gmail.com>
2023-01-11 22:01:11 -0800

    Cors

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var db = require("./queries")
 
 
 const app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.options('*', cors())
 app.use(bodyParser.json())
 app.use(
